520e8a4f03be6adea0a9082989e51246d5345450
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-11-04 17:31:30 +0900

    1일차 - 헤더 및 패킷 구조 설계

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import net from 'net';
 import { HOST, PORT } from './constants/env.js';
-import { onConnection } from './constants/events/onConnection.js';
+import { onConnection } from './events/onConnection.js';
 
 const server = net.createServer(onConnection);
 
