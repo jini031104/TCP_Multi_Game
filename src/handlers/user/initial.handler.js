5d62befd0705850c6e2cabe0c6af02aff848e1f0
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-11-05 04:02:30 +0900

    4일차 - 접속 시 생성 이벤트

--- src/handlers/user/initial.handler.js
+++ src/handlers/user/initial.handler.js
@@ -1,5 +1,7 @@
+import { HANDELR_IDS, RESPONSE_SUCCESS_CODE } from '../../constants/handlerIds.js';
 import { getGameSession } from '../../session/game.session.js';
 import { addUser } from '../../session/user.session.js';
+import { createResponse } from '../../utils/response/createResponse.js';
 
 const initialHandler = ({ socket, userId, payload }) => {
     try {
@@ -8,6 +10,12 @@ const initialHandler = ({ socket, userId, payload }) => {
         const gameSession = getGameSession();
 
         gameSession.addUser(user);
+
+        const initialResponse = createResponse(HANDELR_IDS.INITIAL, RESPONSE_SUCCESS_CODE, {
+            userId: deviceId,
+        });
+
+        socket.write(initialResponse);
     } catch (error) {
         console.error(error);
     }
