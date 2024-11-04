2b0def90e7c5829c282d6df3cc7d0c1379b56566
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-11-04 17:08:57 +0900

    1일차 - 소켓 이벤트 확인 / 기능별 폴더 & 파일 분리

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import net from 'net';
 import { HOST, PORT } from './constants/env.js';
+import { onConnection } from './constants/events/onConnection.js';
 
-const server = net.createServer();
+const server = net.createServer(onConnection);
 
 server.listen(PORT, HOST, () => {
     console.log(`서버가 실행 중이다: ${HOST}:${PORT}`);
