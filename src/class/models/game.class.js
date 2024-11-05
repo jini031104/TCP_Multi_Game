098eea7830bd967b4e6723c1d7a30c750228a4da
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-11-06 03:25:25 +0900

    8일차 - (서버) 레이턴시 매니저, 추측항법 적용

--- src/class/models/game.class.js
+++ src/class/models/game.class.js
@@ -1,13 +1,16 @@
 import { createLocationPacket } from '../../utils/notification/game.notification.js';
+import LatencyManager from '../managers/latency.manager.js';
 
 class Game {
     constructor(id) {
         this.id = id;
         this.users = [];
+        this.latencyManager = new LatencyManager();
     }
 
     addUser(user) {
         this.users.push(user);
+        this.latencyManager.addUser(user.id, user.ping.bind(user), 1000);
     }
 
     getUser(userId) {
@@ -17,18 +20,33 @@ class Game {
     removeUser(socket) {
         const index = this.users.findIndex((user) => user.socket === socket);
         if (index !== -1) {
+            if (this.users.length === 1) {
+                this.latencyManager.clearAll();
+            }
+            this.latencyManager.removeUser(this.users[index].id);
             return this.users.splice(index, 1)[0];
         }
     }
 
+    getMaxLatency() {
+        let maxLatency = 0;
+        this.users.forEach((user) => {
+            maxLatency = Math.max(maxLatency, user.latency);
+        });
+
+        return maxLatency;
+    }
+
     // 나(userId)를 제외한 다른 유저들의 위치값...
     getAllLocation(userId) {
+        const maxLatency = this.getMaxLatency();
         const locationData = this.users
             .filter((user) => {
                 return user.id !== userId;
             })
             .map((user) => {
-                return { id: user.id, playerId: user.playerId, x: user.x, y: user.y };
+                const { x, y } = user.calculatePosition(maxLatency);
+                return { id: user.id, playerId: user.playerId, x, y };
             });
         return createLocationPacket(locationData);
     }
