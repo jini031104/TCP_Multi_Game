4f18a34cb074236e33cbe2a29b0491c59157c8cb
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-11-06 13:09:31 +0900

    연결 종료 시 주소 및 포트 확인

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -2,7 +2,7 @@ import { getGameSession } from '../session/game.session.js';
 import { removeUser } from '../session/user.session.js';
 
 export const onEnd = (socket) => async (err) => {
-    console.log('클라이언트 연결이 종료됐다.');
+    console.log(`클라이언트 연결이 종료됐다: ${socket.remoteAddress}:${socket.remotePort}`);
 
     await removeUser(socket);
 
